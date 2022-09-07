f15e5656cb677ca0b68d982cb5bbbb8fb4afa53d
Victor Menezes Barreto <victormbarreto@hotmail.com>
2022-09-07 17:49:26 -0300

    mensagem 2

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,8 +3,14 @@ let email
 let server
 let age
 let place
-name = Victor
-email = victormbarreto@hotmail.com
-server = Public
-age = 33
-place = plitvice
+name = "Victor"
+email = "victormbarreto@hotmail.com"
+server = "Public"
+age = "33"
+place = "plitvice"
+console.log(name)
+console.log(email)
+console.log(server)
+console.log(age)
+console.log(place)
+console.log (name + email)
